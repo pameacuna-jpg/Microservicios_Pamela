7284908099d5f57aa40e40afc799a6c90ede532e
Dan1elCastroC <danie.castroc@duocuc.cl>
2026-05-22 19:17:23 -0400

    Delete Castro/atencionclinica directory

--- Castro/atencionclinica/src/main/java/com/vetnova/atencionclinica/repository/AtencionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vetnova.atencionclinica.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.vetnova.atencionclinica.model.Atencion;
-
-public interface AtencionRepository extends JpaRepository<Atencion, Long> {
-    
-}
