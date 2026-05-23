a30dee360558e7526aba78d8c528d567272592fb
T28 <danie.castroc@duocuc.cl>
2026-05-23 15:22:47 -0400

    conection service

--- Castro/atencionclinica/atencionclinica/src/main/java/com/vetnova/atencionclinica/AtencionclinicaApplication.java
+++ Castro/atencionclinica/atencionclinica/src/main/java/com/vetnova/atencionclinica/AtencionclinicaApplication.java
@@ -2,12 +2,14 @@ package com.vetnova.atencionclinica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients; 
 
 @SpringBootApplication
+@EnableFeignClients 
 public class AtencionclinicaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AtencionclinicaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AtencionclinicaApplication.class, args);
+    }
 
 }
--- Castro/atencionclinica/atencionclinica/src/main/java/com/vetnova/atencionclinica/client/MascotaClient.java
+++ Castro/atencionclinica/atencionclinica/src/main/java/com/vetnova/atencionclinica/client/MascotaClient.java
@@ -1,14 +1,14 @@
 package com.vetnova.atencionclinica.client;
 
-import com.vetnova.atencionclinica.dto.MascotaDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// Asumimos que Mascotas correrá en el puerto 8082
-@FeignClient(name = "mascotas-service", url = "http://localhost:8082/api/v1/mascotas")
+// Apunta correctamente al puerto 8085 del Microservicio de Mascotas
+@FeignClient(name = "mascota-service", url = "http://localhost:8085/api/v1/mascotas")
 public interface MascotaClient {
 
-    @GetMapping("/{id}")
-    MascotaDTO obtenerMascotaPorId(@PathVariable("id") Long id);
+    // Este es el método exacto que tu FichaClinicaService está buscando
+    @GetMapping("/validar/{id}")
+    boolean validarMascota(@PathVariable("id") Long id);
 }
