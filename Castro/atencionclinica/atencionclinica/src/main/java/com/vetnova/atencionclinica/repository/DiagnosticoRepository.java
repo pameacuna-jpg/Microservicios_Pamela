3c776737bf118a3e1b0e0f83b68dbb8f7a875b88
Pamelaacuna <pame.acuna@duocuc.cl>
2026-05-24 18:36:36 -0400

    CARPETAS DUPLICADAS ELIMINADAS

--- Castro/atencionclinica/atencionclinica/src/main/java/com/vetnova/atencionclinica/repository/DiagnosticoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vetnova.atencionclinica.repository;
-
-import com.vetnova.atencionclinica.model.Diagnostico;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DiagnosticoRepository extends JpaRepository<Diagnostico, Long> {
-}
--- Castro/ventas/ventas/src/main/java/com/vetnova/ventas/repository/VentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vetnova.ventas.repository;
-
-import com.vetnova.ventas.model.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VentaRepository extends JpaRepository<Venta, Long> {
-}
